743470e1fb59b8657c98ef9e902a2e500a15077e
Stanislav Sazanovets <137287700+fNgodshow@users.noreply.github.com>
2025-03-14 12:43:04 +0300

    feat: updating the application

--- js/card-app.js
+++ js/card-app.js
@@ -1,81 +1,242 @@
-(() => {
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
 
-    function shuffle(array) {
-        for (let i = array.length - 1; i > 0; i--) {
-            let j = Math.floor(Math.random() * (i + 1));
-            [array[i], array[j]] = [array[j], array[i]];
-        }
+const appOptions = document.getElementById("app-options");
+const cardApp = document.getElementById("card-app");
+const endGameApp = document.getElementById("end-game");
+const restartApp = document.getElementById("restart-game");
+
+const containerOptions = document.createElement("div");
+containerOptions.classList.add("container-options");
+
+const optionsTitle = document.createElement("h2");
+optionsTitle.textContent = "Choose the difficulty level";
+
+appOptions.append(containerOptions);
+
+const gameField = document.createElement("div");
+gameField.classList.add("game-field");
+
+const difficultyLevels = {
+  easy: 10,
+  medium: 16,
+  hard: 24,
+};
+
+const cardsInfo = [];
+const cardsNumbers = [];
+
+const continueGame = document.getElementById("continue-game");
+const newGame = document.getElementById("new-game");
+
+if (localStorage.getItem("cardsInfo")) {
+  restartApp.classList.toggle("d-none");
+  continueGame.addEventListener("click", continueGames);
+  newGame.addEventListener("click", startNewGame);
+} else {
+  gameOptions(difficultyLevels);
+}
+
+function continueGames() {
+  restartApp.classList.toggle("d-none");
+  cardApp.classList.toggle("d-none");
+  gameField.classList.toggle(localStorage.getItem("dataLevel"));
+  const restartGame = JSON.parse(localStorage.getItem("cardsInfo"));
+  for (let restartCard of restartGame) {
+    if (restartCard.status === "open") {
+      restartCard.status = "close";
+    }
+    cardsInfo.push(restartCard);
+  }
+
+  createGameField(cardsInfo);
+}
+
+function gameOptions(difficultyLevels) {
+  containerOptions.append(optionsTitle);
+  appOptions.classList.toggle("d-none");
+  for (let key in difficultyLevels) {
+    const buttons = document.createElement("button");
+    buttons.setAttribute("data-id", difficultyLevels[key]);
+    buttons.setAttribute("data-level", key);
+    buttons.innerText = key;
+    buttons.classList.add("btn");
+    buttons.addEventListener("click", selectOption);
+    containerOptions.append(buttons);
+  }
+}
+
+function selectOption(e) {
+  if (e.target.getAttribute("data-level") == "easy") {
+    contCell = e.target.getAttribute("data-id");
+    window.localStorage.setItem("dataLevel", "easy");
+    gameField.classList.add("easy");
+  }
+  if (e.target.getAttribute("data-level") == "medium") {
+    contCell = e.target.getAttribute("data-id");
+    window.localStorage.setItem("dataLevel", "medium");
+    gameField.classList.add("medium");
+  }
+  if (e.target.getAttribute("data-level") == "hard") {
+    contCell = e.target.getAttribute("data-id");
+    window.localStorage.setItem("dataLevel", "hard");
+    gameField.classList.add("hard");
+  }
+  appOptions.classList.toggle("d-none");
+  cardApp.classList.toggle("d-none");
+  createNumberCards(contCell);
+  createCardsInfo(cardsNumbers);
+  createGameField(cardsInfo);
+}
+
+function createNumberCards(contCell) {
+  for (let i = 1; i <= contCell; i++) {
+    cardsNumbers.push(i);
+    cardsNumbers.push(i);
+  }
+  shuffle(cardsNumbers);
+}
+
+function createCardsInfo(cardsNumbers) {
+  for (let i = 0; i < cardsNumbers.length; i++) {
+    const info = {
+      id: i + 1,
+      number: cardsNumbers[i],
+      status: "close",
+    };
+    cardsInfo.push(info);
+  }
+}
+
+function createGameField(cardsInfo) {
+  gameField.innerHTML = "";
+  cardsInfo.forEach((cardInfo) => {
+    const card = document.createElement("div");
+    card.classList.add("game-card");
+
+    const flipCardInner = document.createElement("div");
+    flipCardInner.classList.add("flip-card-inner");
+
+    const flipCardFront = document.createElement("div");
+    flipCardFront.classList.add("flip-card-front");
+    flipCardFront.innerHTML = `<img src="./image/shirt.png" alt="Avatar" style="width: 105px; height: 150px" />`;
+
+    const flipCardBack = document.createElement("div");
+    flipCardBack.classList.add("flip-card-back");
+
+    card.setAttribute("data-number", `${cardInfo.number}`);
+    card.setAttribute("id", `${cardInfo.id}`);
+    if (cardInfo.status === "open" || cardInfo.status === "completed") {
+      card.classList.add("openCard");
+    } else {
+      card.classList.remove("openCard");
     }
 
-    function createCardApp(container, contCell = 16) {
+    const cardNumber = document.createElement("span");
+    cardNumber.classList.add("cardNumber");
+    cardNumber.innerHTML = cardInfo.number;
 
-        if (contCell % 2) {
-            console.log('error number! use an even number');
-            return;
+    flipCardBack.append(cardNumber);
+    flipCardInner.append(flipCardBack);
+    flipCardInner.append(flipCardFront);
+    card.append(flipCardInner);
+    gameField.append(card);
+
+    card.addEventListener("click", openCard);
+  });
+  cardApp.append(gameField);
+}
+
+function openCard(e) {
+  const _id = e.currentTarget.getAttribute("id");
+  const openCardInfo = (element) => {
+    return element.id == _id;
+  };
+
+  const openCard = cardsInfo.find(openCardInfo);
+
+  if (openCard.status === "completed") {
+    return;
+  }
+  if (openCard.status === "close") {
+    openCard.status = "open";
+    e.currentTarget.classList.add("openCard");
+  }
+
+  const openCards = cardsInfo.filter(function (card) {
+    return card.status === "open";
+  });
+
+  if (openCards.length > 2) {
+    openCards.forEach((card) => {
+      card.status = "close";
+    });
+    openCards.length = 0;
+    createGameField(cardsInfo);
+    return;
+  }
+
+  setTimeout(function () {
+    if (openCards.length === 2) {
+      if (openCards[0].number === openCards[1].number) {
+        for (const card of openCards) {
+          card.status = "completed";
         }
-
-        const cardApp = document.getElementById('card-app');
-
-        const gameField = document.createElement('div');
-        gameField.classList.add('game-field');
-
-        let gameCards;
-
-        let cardNumber;
-
-        let arrCardNumbers = [];
-
-
-        for (let i = 1; i <= contCell / 2; i++) {
-            arrCardNumbers.push(i);
-            arrCardNumbers.push(i);
-        };
-
-        shuffle(arrCardNumbers);
-
-        
-
-        for (let i = 0; i < arrCardNumbers.length; i++) {
-
-            gameCards = document.createElement('div');
-            gameCards.classList.add('game-cards');
-            gameCards.style.backgroundColor = 'black';
-
-            cardNumber = document.createElement('span');
-            cardNumber.classList.add('cardNumber');
-            cardNumber.innerHTML = arrCardNumbers[i];
-
-
-            gameCards.append(cardNumber);
-            gameField.append(gameCards);
-
-            gameCards.addEventListener('click', function () {
-                if (this.style.backgroundColor) {
-                    this.style.backgroundColor = null;
-                } else {
-                    this.style.backgroundColor = 'black';
-                }
-            });
-
+        openCards.length = 0;
+      } else {
+        for (const card of openCards) {
+          card.status = "close";
         }
+        openCards.length = 0;
+      }
 
-        
-
-        
-
-
-
-        cardApp.append(gameField);
-
-
-        
-
-        
-        
+      createGameField(cardsInfo);
     }
+    window.localStorage.setItem("cardsInfo", JSON.stringify(cardsInfo));
+    endGames(cardsInfo);
+  }, 800);
+}
 
+function endGames(cards) {
+  const completedCards = cardsInfo.filter(function (card) {
+    return card.status === "completed" || card.status === "open";
+  });
+  if (completedCards.length === cardsInfo.length) {
+    window.localStorage.removeItem("cardsInfo");
+    cardApp.classList.toggle("d-none");
+    endGameApp.classList.toggle("d-none");
 
+    const endGameContainer = document.createElement("div");
+    endGameContainer.classList.add("end_game-container");
+    const endTitle = document.createElement("h2");
+    endTitle.classList.add("options-title");
+    endTitle.textContent = "Well done";
+    const button = document.createElement("button");
+    button.textContent = "New game";
+    button.classList.add("btn");
+    button.addEventListener("click", startNewGame);
 
-    window.createCardApp = createCardApp;
+    endGameContainer.append(endTitle);
+    endGameContainer.append(button);
+    endGameApp.append(endGameContainer);
+  }
+}
 
-})();
+function startNewGame() {
+  window.localStorage.removeItem("cardsInfo");
+  window.localStorage.removeItem("dataLevel");
+  cardsInfo.length = 0;
+  cardsNumbers.length = 0;
+  containerOptions.innerHTML = "";
+  gameField.classList.remove("easy");
+  gameField.classList.remove("medium");
+  gameField.classList.remove("hard");
+  restartApp.classList.add("d-none");
+  cardApp.classList.add("d-none");
+  endGameApp.classList.add("d-none");
+  gameOptions(difficultyLevels);
+}
